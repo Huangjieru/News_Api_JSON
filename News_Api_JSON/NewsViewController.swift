aac3df0b40c35ae911d5c0e10eca12dc1785d5c3
jru <jieru04@gmail.com>
2022-12-10 21:47:58 +0800

    add  activityIndicator, refreshController, searchBarCancel

--- News_Api_JSON/NewsViewController.swift
+++ News_Api_JSON/NewsViewController.swift
@@ -14,15 +14,23 @@ class NewsViewController:UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     //宣告變數建立NewsController物件
     var newsController = NewsController()
     //存入抓取到的API資料
     var newsArray:[Articles] = [Articles]()
+    //加入searchbar
+//    var searchController:UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "News"
+        //loading資料
+//        activityIndicator.alpha = 1
+        activityIndicator.startAnimating()
         
         //表格代理
         tableView.delegate = self
@@ -30,16 +38,40 @@ class NewsViewController:UIViewController {
         //搜尋代理
         searchBar.delegate = self
         searchBar.placeholder = "search news that you want."
-
+        searchBar.showsCancelButton = true //顯示“取消”按鈕
+        //抓資料
+        fetchNews()
+        refreshControl()
+        
+        //在navigation加入searchbar(也有cancel的按鈕)
+//        searchController = UISearchController(searchResultsController: nil)
+//        navigationItem.searchController = searchController
+    
+    }
+    //抓新聞資料
+    private func fetchNews(){
         newsController.fetchItems { news in
             if let news{
                 DispatchQueue.main.async {
                     self.newsArray = news
                     self.tableView.reloadData()
+                    //抓完資料Activity Indicator元件停止轉動
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.alpha = 0
                 }
             }
         }
-    
+    }
+    //更新資料與停止更新
+    @objc func pullToRefresh(){
+        fetchNews()
+        tableView.refreshControl?.endRefreshing()
+        
+    }
+    //更新
+    func refreshControl(){
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(pullToRefresh), for: .valueChanged)
     }
     //將要呈現在tableView cell裡的資料另外寫成function
     func configure(cell:NewsTableViewCell,forItemAt indexPath: IndexPath){
@@ -48,13 +80,16 @@ class NewsViewController:UIViewController {
         cell.titleLabel.text = news.title
         cell.descriptionLabel.text = news.description
         cell.picImageView.image = UIImage(systemName: "newspaper.fill")
-        newsController.fetchImage(from: news.urlToImage!) { imagePic in
-            if let image = imagePic{
-                DispatchQueue.main.async {
-                    cell.picImageView.image = image
+        if let newsImage = news.urlToImage {
+            newsController.fetchImage(from: newsImage) { imagePic in
+                if let image = imagePic{
+                    DispatchQueue.main.async {
+                        cell.picImageView.image = image
+                    }
                 }
             }
         }
+        
         
         tableView.rowHeight = 180
         cell.titleLabel.font = UIFont.systemFont(ofSize: 25)
@@ -101,6 +136,7 @@ extension NewsViewController: UITableViewDelegate, UITableViewDataSource{
         if let newsDetail = newsArray[indexPath.row].url{
             let safariViewController = SFSafariViewController(url: newsDetail)
             present(safariViewController, animated: true)
+           
         }
         
     }
@@ -108,9 +144,7 @@ extension NewsViewController: UITableViewDelegate, UITableViewDataSource{
 }
 extension NewsViewController: UISearchBarDelegate{
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-
-        self.newsArray = []
+    func fetchSearchNews(){
         newsController.fetchSearchNews(text: searchBar.text ?? "") { news in
             if let news{
                 DispatchQueue.main.async {
@@ -119,7 +153,39 @@ extension NewsViewController: UISearchBarDelegate{
                 }
             }
         }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        self.newsArray = []
+        
+        fetchSearchNews()
+        refreshSearchController()
+        
         view.endEditing(true)
+    }
+    
+    //下拉更新重新抓艘尋的茲料
+    @objc private func pullSearchToRefresh(){
+        fetchSearchNews()
+        tableView.refreshControl?.endRefreshing()
+    }
+    //更新搜尋的新聞內容
+   private func refreshSearchController(){
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(pullSearchToRefresh), for: .valueChanged)
+    }
+    //searchBar按cancel按鈕
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.endEditing(true)
+        //清除陣列裡的資料
+        newsArray.removeAll()
+        //下方重新抓取資料並顯示
+        fetchNews()
+        tableView.reloadData()
     }
    
 }
+
+
